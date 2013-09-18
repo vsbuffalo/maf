f6f77436b4ede4e137cb6cfbd14ba6c4b379d31e
Vince Buffalo <vsbuffaloAAAAAA@gmail.com>
2013-09-18 10:49:34 -0700

    modified slurm script

--- scripts/slurm.sh
+++ scripts/slurm.sh
@@ -17,8 +17,8 @@ check_in_path bwa
 
 IN1=simreads/chr2-chr10-01.bwa.read1.fastq
 IN2=simreads/chr2-chr10-01.bwa.read2.fastq
-MAFPY=python /home/vince251/projects/maf/maf/maf.py
+MAFPY=/home/vince251/projects/maf/maf/maf.py
 
 mkdir -p log alns eval time
 
-$MAFPY map -o mafrun-1.txt -1 $IN1 -2 $IN2 bowtie2.cfg bwa-mem.cfg novoalign.cfg | sed -n "$SLURM_ARRAY_TASK_ID"p | bash
+python $MAFPY map -o mafrun-1.txt -1 $IN1 -2 $IN2 bowtie2.cfg bwa-mem.cfg novoalign.cfg | sed -n "$SLURM_ARRAY_TASK_ID"p | bash
